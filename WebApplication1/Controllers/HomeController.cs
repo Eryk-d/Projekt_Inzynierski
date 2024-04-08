7f3c551ebb737e701e543ea977f1a2b219990a16
Eryk Delikat <168139@o365.prz.edu.pl>
2024-04-08 19:54:11 +0200

    dodano nowe zakladke

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -66,6 +66,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult Dodaj_haslo()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
